c2cfa9251b18dafd2601f29a120a4c8ce3df60a1
Mohd Abdullah <mohammadabdullah2jan@gmail.com>
2022-11-19 22:13:14 +0530

    social-link-changed

--- src/helpers/SocialMediaLinks.js
+++ src/helpers/SocialMediaLinks.js
@@ -3,6 +3,6 @@ export const SocialMediaLinks =
     {
         Github : "https://github.com/mohammadabdullah8674/",
         Instagram : "https://www.instagram.com/i_am_abdullahzubair/",
-        LinkedIn : "https://www.linkedin.com/in/mohd-abdullah-303456215/",
+        LinkedIn : "https://www.linkedin.com/in/mohd-abdullah-zubair/",
         Email : "mohammadabdullah2jan@gmail.com"
     }
